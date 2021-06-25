e748ff373d33e7d8cf6ed20f89175dda23c588b5
Divya Prajapati <cs19b067@smail.iitm.ac.in>
2021-06-25 17:24:35 +0530

    Jethalal

--- main.cpp
+++ main.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int main()
 {
     cout << "Hello kem chho !" << endl;
+    cout << "reply : Majama .." << endl;
+    cout << "chal chal vayadi naitar" << endl;
     return 0;
 }
